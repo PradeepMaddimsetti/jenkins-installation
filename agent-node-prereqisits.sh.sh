2d7658f8a022722b139a19decaffb7a943a5ac4f
PradeepMaddimsetti <m.pradeepkumarnaidu@gmail.com>
2021-06-19 08:30:43 +0530

    c4

---
+++ agent-node-prereqisits.sh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+sudo apt-get update  
+sudo apt-get install openjdk-11-jdk -y
+sudo apt-get update
+sudo apt-get install maven -y
+java -version
+mvn -version
+
--- pk.sh
+++
@@ -1,5 +0,0 @@
-while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
-    openjdk-11-jdk
-    maven    
-EOF
-)
