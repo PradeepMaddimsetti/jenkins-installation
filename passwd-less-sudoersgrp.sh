cc32b1c6f22068fc3e1148bfc5b8107fd48848d1
PradeepMaddimsetti <m.pradeepkumarnaidu@gmail.com>
2021-07-21 09:17:30 +0530

    modified the sudoers file

--- passwd-less-sudoersgrp.sh
+++ passwd-less-sudoersgrp.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "Enter the user name which we want to add passwd less  athuntication for sudo commends: "
 read -e username
-sudo sed '/%sudo/ a '$username' ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
+sudo sed -i '/%sudo/ a '$username' ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
